3f4f289a7880c498d7e4b1ad4a2c5dce785e3cb9
enducker <davidkuiper@gmail.com>
2017-09-05 17:33:55 -0400

    Remove CSS, add styled-components

--- src/App.js
+++ src/App.js
@@ -2,20 +2,29 @@ import React, { Component } from 'react';
 import logo from './logo.png';
 import FactomUtil from './factomjsutil';
 import styled from 'styled-components';
-import './App.css';
 
-const PageTitle = styled.h2`
-  color: orange;
+const Header = styled.div`
+background-color: rgb(17, 30, 51);
+padding: 20px;
+color: white;
+display: flex;
+align-items: center;
+font-size: 2em;
+`;
+
+const HeaderLogo = styled.img`
+  height: 50px;
+  margin-right: 10px;
 `;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <PageTitle>MyFactomWallet</PageTitle>
-        </div>
+        <Header>
+          <HeaderLogo src={logo} alt="logo"/>
+           MyFactomWallet
+        </Header>
       </div>
     );
   }
