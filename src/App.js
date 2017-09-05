3365940c0c791ffdc85dc44d8eb6e95dd55d1b58
enducker <davidkuiper@gmail.com>
2017-09-05 19:30:13 -0400

    Attempt to integrate factomjs-util

--- src/App.js
+++ src/App.js
@@ -2,6 +2,27 @@ import React, { Component } from 'react';
 import logo from './logo.png';
 import styled from 'styled-components';
 
+//---------------------------Pull from NODE_MODULES----------------------------------------------
+//1. Uncomment the next line and run "npm start".  This will work.
+import factomUtil from 'factomjs-util'; //OR var factomUtil = require('factomjs-util');
+//The above example fails when running "npm run build" because of a failure to minify
+//Note it's possible to import the function directly with => import {isValidAddress} from 'factomjs-util';
+
+//2. Instead, uncomment the next line and run "npm start" to use the pre-compiled version.
+//import factomUtil from 'factomjs-util/dist/factomjs-util'; //OR var factomUtil = require('factomjs-util/dist/factomjs-util');
+//This compiles but errors when calling "isValidAddress" saying it is not a function.
+//Why can't we call functions from the pre-compiled version??
+//---------------------------Pull from NODE_MODULES----------------------------------------------
+
+//---------------------------Pull from /src------------------------------------------------------
+//3. This is a precompiled version (with compile errors fixed) and results in the same error as #2
+//import factomUtil from './sourcefactomjs-util';
+//---------------------------Pull from /src------------------------------------------------------
+
+
+const validAddress = ('Fs2S5Ut7ZZhsgFuko2BNsGDMmFaNsX46cVnsE5Y2AmgQ2C31e9DX');
+const isValid = factomUtil.isValidAddress(validAddress);
+
 const Header = styled.div`
 background-color: rgb(17, 30, 51);
 padding: 20px;
@@ -24,6 +45,7 @@ class App extends Component {
           <HeaderLogo src={logo} alt="logo"/>
            MyFactomWallet
         </Header>
+        {validAddress} = <b>{isValid ? 'true' : 'false'}</b>
       </div>
     );
   }
