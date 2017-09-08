8d5e57db34d529385a095cd5c2cfd2ce0de41492
enducker <davidkuiper@gmail.com>
2017-09-07 20:33:49 -0400

    Test Zeplin Styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.png';
+import logo from './headerLogo.png';
 import styled from 'styled-components';
 
 import factomUtil from 'factomjs-util/dist/factomjs-util';
@@ -9,29 +9,63 @@ const validAddress = ('Fs2S5Ut7ZZhsgFuko2BNsGDMmFaNsX46cVnsE5Y2AmgQ2C31e9DX');
 const isValid = factomUtil.isValidAddress(validAddress);
 
 const Header = styled.div`
-background-color: rgb(17, 30, 51);
-padding: 20px;
-color: white;
-display: flex;
-align-items: center;
-font-size: 2em;
+  padding-left:82.9px;
+  width: 284.6px;
+  height: 24px;
+  font-family: Montserrat;
+  font-size: 20px;
+  font-weight: bold;
+  text-align: left;
+  color: #ffffff;
 `;
 
 const HeaderLogo = styled.img`
-  height: 50px;
-  margin-right: 10px;
+  width: 39.1px;
+  height: 31.4px;
+  margin-top:30px;
+  margin-right:13px;
+  float:left;
+`;
+
+const Outer = styled.div`
+  width: 1440px;
+  height: 90px;
+  background-color: #001830;
+`;
+
+const Main = styled.div`
+  width: 1440px;
+  height: 939px;
+  transform: rotate(-180deg);
+  background-image: linear-gradient(to bottom, #001830, #002347);
+`;
+
+const Title = styled.div`
+  padding-top:35px;
+`;
+
+const POC = styled.div`
+  transform: rotate(180deg);
+  color: orange;
+  position: fixed;
+  bottom: 1px;
+  right: 1px;
 `;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <span>
+      <Outer>
         <Header>
           <HeaderLogo src={logo} alt="logo"/>
-           MyFactomWallet
+           <Title>MyFactomWallet</Title>
         </Header>
-        {validAddress} = <b>{isValid ? 'true' : 'false'}</b>
-      </div>
+      </Outer>
+      <Main>
+        <POC>{validAddress} = <b>{isValid ? 'true' : 'false'}</b></POC>
+      </Main>
+    </span>
     );
   }
 }
