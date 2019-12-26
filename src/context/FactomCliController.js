97f03dfd60dd42804abbce43f324c7a71ab1f86d
Alex G <ahgoehring@gmail.com>
2019-12-26 14:46:29 -0500

    Enhancement: Get new directory block from FactomEventEmitter (#13)
    Enhancement: Get new directory block from FactomEventEmitter

--- src/context/FactomCliController.js
+++ src/context/FactomCliController.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FactomCliContext } from './FactomCliContext';
-import { FactomCli } from 'factom/dist/factom';
+import { FactomCli, FactomEventEmitter } from 'factom/dist/factom';
 import { withNetwork } from './NetworkContext';
 import defaultsDeep from 'lodash/fp/defaultsDeep';
 import _flowRight from 'lodash/flowRight';
@@ -35,27 +35,8 @@ class FactomCliController extends React.Component {
 	async componentDidMount() {
 		await this.connectToServer();
 
-		this.blockHeightTimerId = setInterval(this.updateBlockHeight, 60000);
-
 		await this.smartSetState({ isStateHydrated: true });
 	}
-
-	componentWillUnmount() {
-		clearInterval(this.blockHeightTimerId);
-	}
-
-	updateBlockHeight = async () => {
-		const result = await this.state.factomCli.getDirectoryBlockHead();
-
-		const { height } = result;
-
-		if (height !== this.state.blockHeight) {
-			// process new block
-			this.setState({
-				blockHeight: height,
-			});
-		}
-	};
 
 	getDefaultConnectionParams = () => {
 		const connectionParams = {
@@ -69,17 +50,41 @@ class FactomCliController extends React.Component {
 	newFactomCli = (connectionParams) =>
 		new FactomCli(defaultsDeep(this.defaultConnectionParams, connectionParams));
 
+	newFactomEmitter = (factomCli) =>
+		new FactomEventEmitter(factomCli, {
+			interval: 10000,
+		});
+
+	updateBlock = (directoryBlock) => {
+		const { height } = directoryBlock;
+		// process new block
+		this.setState({
+			blockHeight: height,
+		});
+	};
+
 	connectToServer = async () => {
+		if (this.state.factomEmitter) {
+			this.state.factomEmitter.removeListener(
+				'newDirectoryBlock',
+				this.updateBlock
+			);
+		}
+
 		const connectionParams = {
 			host: this.props.networkController.networkProps.apiHost,
 			port: this.props.networkController.networkProps.apiPort,
 		};
 
+		const factomCli = this.newFactomCli(connectionParams);
+		const factomEmitter = this.newFactomEmitter(factomCli);
+
 		await this.smartSetState({
-			factomCli: this.newFactomCli(connectionParams),
+			factomCli,
+			factomEmitter,
 		});
 
-		await this.updateBlockHeight();
+		this.state.factomEmitter.on('newDirectoryBlock', this.updateBlock);
 	};
 
 	smartSetState = (newState, afterSetState = _noop) =>
