29b3e0dc5217f22f667a499c5c2ed378b6005932
Dennis Bunfield <dbunfield@aegistg.com>
2021-03-06 00:31:39 -0600

    Updates

---
+++ precache-manifest.4313b5bdf3cb294a621c2149cec362a0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4841b5d43236c1aceb40ed28cb42ee0b",
+    "url": "/MyFactomWallet/index.html"
+  },
+  {
+    "revision": "60191acf9dfaf39cea37",
+    "url": "/MyFactomWallet/static/css/main.0e83b11b.chunk.css"
+  },
+  {
+    "revision": "e1a25fa891192725b921",
+    "url": "/MyFactomWallet/static/js/2.8c3658f4.chunk.js"
+  },
+  {
+    "revision": "0f39d03121d18b6277abffbfab3cad75",
+    "url": "/MyFactomWallet/static/js/2.8c3658f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60191acf9dfaf39cea37",
+    "url": "/MyFactomWallet/static/js/main.81b8c163.chunk.js"
+  },
+  {
+    "revision": "e1481fbb1068e61a18da",
+    "url": "/MyFactomWallet/static/js/runtime-main.eb20618e.js"
+  },
+  {
+    "revision": "e79a5c9c8039fc95d090ad40e32ba0fc",
+    "url": "/MyFactomWallet/static/media/headerLogo.e79a5c9c.png"
+  },
+  {
+    "revision": "e1222ceffcac3b56a35deed4dab75fc9",
+    "url": "/MyFactomWallet/static/media/ledgerLogo.e1222cef.svg"
+  }
+]);
